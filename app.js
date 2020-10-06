1474a9a14d72c3dcaac524ae6da6ecfc58b22f20
ssshoshi <jlschultz88@gmail.com>
2020-10-06 15:47:07 -0400

    fix prettier format

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var express = require("express"),
   LocalStrategy = require("passport-local"),
   User = require("./models/user");
 
-var url = process.env.DATABASEURL || "mongodb://localhost/workreducepay/";
+var url = process.env.DATABASEURL || "mongodb://localhost/workreducepay";
 mongoose.connect(url, { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
